1e89243fee9ec7c11c57c8dc4489ee881dc3aca1
krios2146 <krios2146@gmail.com>
2024-03-13 16:53:38 +0700

    feat: add right shift to the slash

--- keyboards/ktec/ergodone/keymaps/krios2146/keymap.c
+++ keyboards/ktec/ergodone/keymaps/krios2146/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MINUS,           KC_6,  KC_7,      KC_8,      KC_9,     KC_0,          KC_DELETE,
         KC_LEFT_BRACKET,    KC_Y,  KC_U,      KC_I,      KC_O,     KC_P,          KC_MINUS,
                             KC_H,  KC_J,      KC_K,      KC_L,     KC_SEMICOLON,  KC_EQUAL,
-        KC_RIGHT_BRACKET,   KC_N,  KC_M,      KC_COMMA,  KC_DOT,   KC_SLASH,      KC_F11,
+        KC_RIGHT_BRACKET,   KC_N,  KC_M,      KC_COMMA,  KC_DOT,   MT(MOD_RSFT, KC_SLASH),      KC_F11,
                                    KC_QUOTE,  KC_UP,     KC_DOWN,  KC_NO,         KC_F5,
 
         KC_GRAVE,           KC_INSERT,
