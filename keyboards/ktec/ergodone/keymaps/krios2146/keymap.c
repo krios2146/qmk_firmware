2f91bf301b99364b8534b9589a2c5aabd7800def
krios2146 <krios2146@gmail.com>
2024-03-13 16:51:06 +0700

    feat: add tap dance grave to the esc

--- keyboards/ktec/ergodone/keymaps/krios2146/keymap.c
+++ keyboards/ktec/ergodone/keymaps/krios2146/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: base layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   Esc  |   1  |   2  |   3  |   4  |   5  |  \   |           |  -_  |   6  |   7  |   8  |   9  |   0  | Delete |
+ * | Esc/Grv|   1  |   2  |   3  |   4  |   5  |  \   |           |  -_  |   6  |   7  |   8  |   9  |   0  | Delete |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  | Layer|           |  {   |   Y  |   U  |   I  |   O  |   P  |   -_   |
  * |--------+------+------+------+------+------| SYMB |           |  [   |------+------+------+------+------+--------|
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
         // left hand
-        KC_ESCAPE,      KC_1,         KC_2,      KC_3,     KC_4,         KC_5,       KC_BACKSLASH,
+        ACTION_TAP_DANCE_DOUBLE(KC_ESCAPE, KC_GRAVE),      KC_1,         KC_2,      KC_3,     KC_4,         KC_5,       KC_BACKSLASH,
         KC_TAB,         KC_Q,         KC_W,      KC_E,     KC_R,         KC_T,       TG(SYMB),
         KC_CAPS_LOCK,   KC_A,         KC_S,      KC_D,     KC_F,         KC_G,
         KC_LEFT_SHIFT,  KC_Z,         KC_X,      KC_C,     KC_V,         KC_B,       KC_NO,
--- keyboards/ktec/ergodone/rules.mk
+++ keyboards/ktec/ergodone/rules.mk
@@ -1,5 +1,6 @@
 # custom matrix setup
 CUSTOM_MATRIX = lite
+TAP_DANCE_ENABLE = yes
 
 VPATH += drivers/gpio
 SRC += mcp23018.c matrix.c
