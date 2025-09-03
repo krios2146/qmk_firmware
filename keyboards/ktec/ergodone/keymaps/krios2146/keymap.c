24306ddcc32f4107a8f92ad5633dff3ca739cb67
Vladimir Kidyaev <krios2146@gmail.com>
2025-09-03 10:57:44 +0700

    change layers ordering to remove interference

--- keyboards/ktec/ergodone/keymaps/krios2146/keymap.c
+++ keyboards/ktec/ergodone/keymaps/krios2146/keymap.c
@@ -2,8 +2,8 @@
 #include "version.h"
 
 #define BASE 0
-#define SYMB 1
-#define GAME 2
+#define GAME 1
+#define SYMB 2
 
 enum custom_keycodes {
     COPY = SAFE_RANGE,
@@ -63,6 +63,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGUP,
         KC_PGDN,  KC_ENT,    KC_SPC
 ),
+/* Keymap 1: Gaming Layer
+ *
+ * ┌────────┬─────┬─────┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┬─────┬────────┐
+ * │  Esc   │  1  │  2  │  3  │  4  │  5  │     │       │     │  6  │  7  │  8  │  9  │  0  │   `~   │
+ * ├────────┼─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┼────────┤
+ * │  Tab   │  Q  │  W  │  E  │  R  │  T  │ GA  │       │  [  │  Y  │  U  │  I  │  O  │  P  │   ─_   │
+ * ├────────┼─────┼─────┼─────┼─────┼─────┤ ME  │       │  {  ├─────┼─────┼─────┼─────┼─────┼────────┤
+ * │  Caps  │  A  │  S  │  D  │  F  │  G  ├─────┤       ├─────┤  H  │  J  │  K  │  L  │ :;  │   =+   │
+ * ├────────┼─────┼─────┼─────┼─────┼─────┤     │       │  ]  ├─────┼─────┼─────┼─────┼─────┼────────┤
+ * │ LShift │ Z ^ │  X  │  C  │  V  │  B  │     │       │  }  │  N  │  M  │  <, │ >.^ │ /?⇧ │   \│   │
+ * └─┬──────┼─────┼─────┼─────┼─────┼─────┴─────┘       └─────┴─────┼─────┼─────┼─────┼─────┼──────┬─┘
+ *   │ SYMB │ Win │     │     │ Alt │                               │  "' │     │     │SYMB │      │
+ *   └──────┴─────┴─────┴─────┴─────┘                               └─────┴─────┴─────┴─────┴──────┘
+ *                                  ┌─────┬─────┐       ┌─────┬─────┐
+ *                                  │Copy │Print│       │ Ins │Paste│
+ *                            ┌─────┼─────┼─────┤       ├─────┼─────┼─────┐
+ *                            │     │     │ Home│       │PgUp │     │     │
+ *                            │Space│ Del ├─────┤       ├─────│Enter│ Back│
+ *                            │     │     │ End │       │PgDn │     │     │
+ *                            └─────┴─────┴─────┘       └─────┴─────┴─────┘
+ */
+[GAME] = LAYOUT_ergodox(
+        // Left hand
+        KC_ESC,    KC_1,          KC_2,   KC_3,   KC_4,     KC_5,    KC_NO,
+        KC_TAB,    KC_Q,          KC_W,   KC_E,   KC_R,     KC_T,    TG(GAME),
+        KC_CAPS,   KC_A,          KC_S,   KC_D,   KC_F,     KC_G,
+        KC_LSFT,   LCTL_T(KC_Z),  KC_X,   KC_C,   KC_V,     KC_B,    KC_NO,
+        TT(SYMB),  KC_LEFT_GUI,   KC_NO,  KC_NO,  KC_LALT,
+
+                                                            // Thumb cluster
+                                                            COPY,    KC_PRINT_SCREEN,
+                                                                     KC_HOME,
+                                                  KC_SPC,   KC_DEL,  KC_END,
+
+        // Right hand
+        KC_NO,    KC_6,      KC_7,      KC_8,     KC_9,            KC_0,              KC_GRV,
+        KC_LBRC,  KC_Y,      KC_U,      KC_I,     KC_O,            KC_P,              KC_MINS,
+                  KC_H,      KC_J,      KC_K,     KC_L,            KC_SCLN,           KC_EQL,
+        KC_RBRC,  KC_N,      KC_M,      KC_COMM,  LCTL_T(KC_DOT),  RSFT_T(KC_SLASH),  KC_BSLS,
+                             KC_QUOTE,  KC_NO,    KC_NO,           TT(SYMB),          KC_NO,
+
+        // Thumb cluster
+        KC_INS,   PASTE,
+        KC_PGUP,
+        KC_PGDN,  KC_ENT,   KC_BSPC
+),
 /* Keymap 2: Symbol Layer
  *
  * ┌────────┬─────┬─────┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┬─────┬────────┐
@@ -107,52 +153,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS,
        KC_TRNS,
        KC_TRNS,  KC_NO,    KC_TRNS
-),
-/* Keymap 2: Gaming Layer
- *
- * ┌────────┬─────┬─────┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┬─────┬────────┐
- * │  Esc   │  1  │  2  │  3  │  4  │  5  │     │       │     │  6  │  7  │  8  │  9  │  0  │   `~   │
- * ├────────┼─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┼────────┤
- * │  Tab   │  Q  │  W  │  E  │  R  │  T  │ GA  │       │  [  │  Y  │  U  │  I  │  O  │  P  │   ─_   │
- * ├────────┼─────┼─────┼─────┼─────┼─────┤ ME  │       │  {  ├─────┼─────┼─────┼─────┼─────┼────────┤
- * │  Caps  │  A  │  S  │  D  │  F  │  G  ├─────┤       ├─────┤  H  │  J  │  K  │  L  │ :;  │   =+   │
- * ├────────┼─────┼─────┼─────┼─────┼─────┤     │       │  ]  ├─────┼─────┼─────┼─────┼─────┼────────┤
- * │ LShift │ Z ^ │  X  │  C  │  V  │  B  │     │       │  }  │  N  │  M  │  <, │ >.^ │ /?⇧ │   \│   │
- * └─┬──────┼─────┼─────┼─────┼─────┼─────┴─────┘       └─────┴─────┼─────┼─────┼─────┼─────┼──────┬─┘
- *   │ SYMB │ Win │     │     │ Alt │                               │  "' │     │     │SYMB │      │
- *   └──────┴─────┴─────┴─────┴─────┘                               └─────┴─────┴─────┴─────┴──────┘
- *                                  ┌─────┬─────┐       ┌─────┬─────┐
- *                                  │Copy │Print│       │ Ins │Paste│
- *                            ┌─────┼─────┼─────┤       ├─────┼─────┼─────┐
- *                            │     │     │ Home│       │PgUp │     │     │
- *                            │Space│ Del ├─────┤       ├─────│Enter│ Back│
- *                            │     │     │ End │       │PgDn │     │     │
- *                            └─────┴─────┴─────┘       └─────┴─────┴─────┘
- */
-[GAME] = LAYOUT_ergodox(
-        // Left hand
-        KC_ESC,    KC_1,          KC_2,   KC_3,   KC_4,     KC_5,    KC_NO,
-        KC_TAB,    KC_Q,          KC_W,   KC_E,   KC_R,     KC_T,    TG(GAME),
-        KC_CAPS,   KC_A,          KC_S,   KC_D,   KC_F,     KC_G,
-        KC_LSFT,   LCTL_T(KC_Z),  KC_X,   KC_C,   KC_V,     KC_B,    KC_NO,
-        TT(SYMB),  KC_LEFT_GUI,   KC_NO,  KC_NO,  KC_LALT,
-
-                                                            // Thumb cluster
-                                                            COPY,    KC_PRINT_SCREEN,
-                                                                     KC_HOME,
-                                                  KC_SPC,  KC_DEL,  KC_END,
-
-        // Right hand
-        KC_NO,    KC_6,      KC_7,      KC_8,     KC_9,            KC_0,              KC_GRV,
-        KC_LBRC,  KC_Y,      KC_U,      KC_I,     KC_O,            KC_P,              KC_MINS,
-                  KC_H,      KC_J,      KC_K,     KC_L,            KC_SCLN,           KC_EQL,
-        KC_RBRC,  KC_N,      KC_M,      KC_COMM,  LCTL_T(KC_DOT),  RSFT_T(KC_SLASH),  KC_BSLS,
-                             KC_QUOTE,  KC_NO,    KC_NO,           TT(SYMB),          KC_NO,
-
-        // Thumb cluster
-        KC_INS,   PASTE,
-        KC_PGUP,
-        KC_PGDN,  KC_ENT,   KC_BSPC
 ),
 };
 
